a6578da45c42105bcaeb5b64a5ad09caf32cd5b4
SammyK <sammyk@sammykmedia.com>
2014-05-06 15:53:03 -0500

    Fixed bug that blindly appended params to a url

--- tests/FacebookRequestTest.php
+++ tests/FacebookRequestTest.php
@@ -22,4 +22,34 @@ class FacebookRequestTest extends PHPUnit_Framework_TestCase
     $this->assertNotNull($response->getProperty('name'));
   }
 
+  public function testGracefullyHandlesUrlAppending()
+  {
+    $params = array();
+    $url = 'https://www.foo.com/';
+    $processed_url = FacebookRequest::appendParamsToUrl($url, $params);
+    $this->assertEquals('https://www.foo.com/', $processed_url);
+
+    $params = array(
+      'access_token' => 'foo',
+    );
+    $url = 'https://www.foo.com/';
+    $processed_url = FacebookRequest::appendParamsToUrl($url, $params);
+    $this->assertEquals('https://www.foo.com/?access_token=foo', $processed_url);
+
+    $params = array(
+      'access_token' => 'foo',
+      'bar' => 'baz',
+    );
+    $url = 'https://www.foo.com/?foo=bar';
+    $processed_url = FacebookRequest::appendParamsToUrl($url, $params);
+    $this->assertEquals('https://www.foo.com/?access_token=foo&bar=baz&foo=bar', $processed_url);
+
+    $params = array(
+      'access_token' => 'foo',
+    );
+    $url = 'https://www.foo.com/?foo=bar&access_token=bar';
+    $processed_url = FacebookRequest::appendParamsToUrl($url, $params);
+    $this->assertEquals('https://www.foo.com/?access_token=bar&foo=bar', $processed_url);
+  }
+
 }
