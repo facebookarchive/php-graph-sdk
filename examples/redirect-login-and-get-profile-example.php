3de1b852b37b4bdd9a57098241404badc967c058
Gael Abadin <gael.abadin@gmail.com>
2014-05-13 16:05:22 +0200

    Update and rename fbsdkexample.php to redirect-login-and-get-profile-example.php
    Edit on comments and title for clarity to emphasize it's a redirect login and get profile example and remove some redundancy.
    
    Rename of the script for a more accurate statement of what it exemplifies.

--- examples/redirect-login-and-get-profile-example.php
+++ examples/redirect-login-and-get-profile-example.php
@@ -1,38 +1,32 @@
 <?php
 /**
- * 
- * Facebook PHP SDK v4 login / logout / get profile demo script
- * 
- * This is an extension to the provided "quick start" examples to allow a better understanding of the API.
- * 
+ *
+ * Facebook PHP SDK v4 redirect login / logout / get profile demo script
+ *
+ * Extension of the provided quick start code snippets to allow a better understanding of the SDK's basic functionality.
+ *
  * Set up:
- * 
+ *
  *  - Download and extract the latest release of SDK (https://github.com/facebook/facebook-php-sdk-v4/releases)
- *    on a public folder of your web server.
- * 
+ *    or clone the repo on a public folder of your web server.
+ *
  *  - This script should be on the 'examples' subfolder of the project
- * 
+ *
  *  - Before running this script you must install the vendor dependencies using composer (http://getcomposer.org)
- * 
- *    To do so, you can run these commands on the SDK deployment's main directory (e.g. facebook-php-sdk-v4):
- * 
- *       curl -sS https://getcomposer.org/installer | php
- * 
- *       php composer.phar install
- * 
- *  - Finally: fill the app id, secret and login, logout urls below. 
- * 
- * Don't forget to allow myappdomain.com on your facebook app settings page (You'll find a link to it on 
+ *
+ *  - Finally: fill the app id, secret and login, logout urls below.
+ *
+ * Don't forget to allow myappdomain.com on your facebook app settings page (You'll find a link to it on
  * http://developers.facebook.com/apps where you can also create a new app and find the app's id and secret).
- * 
+ *
  * Usage:
- * 
- *  - Point your browser to http://whatever.myappdomain.com/path/to/fbsdkexample.php
- * 
+ *
+ *  - Point your browser to http://whatever.myappdomain.com/path/to/redirect-login-and-get-profile-example.php
+ *
  * PHP >= 5.4 IS REQUIRED TO RUN VERSION 4.X.X OF Facebook PHP SDK
- * 
+ *
  */
-    
+
 use Facebook\FacebookRedirectLoginHelper;
 use Facebook\FacebookRequest;
 use Facebook\FacebookRequestException;
@@ -40,23 +34,17 @@ use Facebook\FacebookSession;
 
 session_start();
 
-/* CHECK THIS PATH IS CORRECT */
 require_once '../vendor/autoload.php';
 
-/* 
- * EDIT THE FOLLOWING 4 FIELDS BEFORE RUNNING THIS SCRIPT 
- *     - myappdomain.com MUST BE ALLOWED on the facebbok app settings page 
- *     (That's https://developers.facebook.com/apps/{$appId}/settings/)
- */
-
-// your login url (in this example fbsdkexample.php URL)
-$loginRedirectUrl = 'http://whatever.myappdomain.com/path/to/fbsdkexample.php';
-// your logout url (in this example fbsdkexample.php?logout=whatever URL)
-$logoutRedirectUrl = 'http://whatever.myappdomain.com/path/to/fbsdkexample.php?logout=true';
-// your app id
+// login url (in this example, the URL pointing to this script)
+$loginRedirectUrl = 'http://whatever.myappdomain.com/path/to/redirect-login-and-get-profile-example.php';
+// logout url (in this example, the URL pointing to this script, with logout param set)
+$logoutRedirectUrl = 'http://whatever.myappdomain.com/path/to/redirect-login-and-get-profile-example.php?logout=true';
+// app id
 $appId = '';
-// your app secret
+// app secret
 $appSecret = '';
+
 // comma separated list of requested permissions (on top of public profile)
 $scope = 'email';
 
@@ -64,11 +52,11 @@ FacebookSession::setDefaultApplication($appId, $appSecret);
 $out = '';
 
 if (isset($_GET['logout'])){
-    
+
     // New session
     $_SESSION = array();
     session_regenerate_id(true);
-    
+
 }
 
 $helper = new FacebookRedirectLoginHelper($loginRedirectUrl);
@@ -102,14 +90,14 @@ try {
         // New session
         $_SESSION = array();
         session_regenerate_id(true);
-        
+
         $out = "<p><a href="
             . $helper->getLoginUrl(
                 array(
-                    'scope' => 
+                    'scope' =>
                         $scope
-                    )
                 )
+            )
             . ">login</a></p>";
 
     }
@@ -117,47 +105,47 @@ try {
 } catch(FacebookRequestException $ex) {
 
     // When Facebook returns an error
-    
+
     $out = "<p>FacebookRequestException: ".$ex->getMessage()."</p>";
-    
+
     // New session
     $_SESSION = array();
     session_regenerate_id(true);
-    
+
     $out .= "<p><a href="
-            . $helper->getLoginUrl(
-                array(
-                    'scope' => 
-                        $scope
-                    )
-                )
-            . ">login</a></p>";
+        . $helper->getLoginUrl(
+            array(
+                'scope' =>
+                    $scope
+            )
+        )
+        . ">login</a></p>";
 
 } catch(Exception $ex) {
 
     // When validation fails or other local issues
-    
+
     $out = "<p>Exception ".$ex->getMessage()."</p>";
-    
+
     // New session
     $_SESSION = array();
     session_regenerate_id(true);
-    
+
     $out .= "<p><a href="
-            . $helper->getLoginUrl(
-                array(
-                    'scope' => 
-                        $scope
-                    )
-                )
-            . ">login</a></p>";
+        . $helper->getLoginUrl(
+            array(
+                'scope' =>
+                    $scope
+            )
+        )
+        . ">login</a></p>";
 
 }
 ?><!doctype html>
 <html>
 <head>
     <title>
-        facebook login / logout / get profile test page
+        facebook redirect login / logout / get profile test page
     </title>
 </head>
 <body>
