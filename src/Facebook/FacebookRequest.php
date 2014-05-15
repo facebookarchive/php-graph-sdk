5754d4c740af29d269030a739b06e5834ff30c2f
Fosco Marotto <fjm@fb.com>
2014-05-15 15:43:17 -0700

    Updating version

--- src/Facebook/FacebookRequest.php
+++ src/Facebook/FacebookRequest.php
@@ -35,7 +35,7 @@ class FacebookRequest
   /**
    * @const string Version number of the Facebook PHP SDK.
    */
-  const VERSION = '4.0.0';
+  const VERSION = '4.0.4';
 
   /**
    * @const string Default Graph API version for requests
