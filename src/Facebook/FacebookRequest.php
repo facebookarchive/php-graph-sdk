cb616218eaaebef76c6b0934e1ada4121e261702
Fosco Marotto <fjm@fb.com>
2014-06-16 14:51:02 -0700

    Updating pending version to 4.0.9

--- src/Facebook/FacebookRequest.php
+++ src/Facebook/FacebookRequest.php
@@ -39,7 +39,7 @@ class FacebookRequest
   /**
    * @const string Version number of the Facebook PHP SDK.
    */
-  const VERSION = '4.0.8';
+  const VERSION = '4.0.9';
 
   /**
    * @const string Default Graph API version for requests
