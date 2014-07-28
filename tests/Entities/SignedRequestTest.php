04bc3e4343d791666090cd73984ff3a1405b2212
Yassine Guedidi <yassine@guedidi.com>
2014-07-29 00:59:36 +0200

    Update SignedRequest tests

--- tests/Entities/SignedRequestTest.php
+++ tests/Entities/SignedRequestTest.php
@@ -23,164 +23,125 @@
  */
 namespace Facebook\Tests\Entities;
 
+use Mockery as m;
 use Facebook\Entities\SignedRequest;
 
 class SignedRequestTest extends \PHPUnit_Framework_TestCase
 {
+  protected $fakeApp;
 
-  public $appSecret = 'foo_app_secret';
-
-  public $rawSignedRequest = 'U0_O1MqqNKUt32633zAkdd2Ce-jGVgRgJeRauyx_zC8=.eyJvYXV0aF90b2tlbiI6ImZvb190b2tlbiIsImFsZ29yaXRobSI6IkhNQUMtU0hBMjU2IiwiaXNzdWVkX2F0IjozMjEsImNvZGUiOiJmb29fY29kZSIsInN0YXRlIjoiZm9vX3N0YXRlIiwidXNlcl9pZCI6MTIzLCJmb28iOiJiYXIifQ==';
-
-  public $payloadData = array(
-    'oauth_token' => 'foo_token',
-    'algorithm' => 'HMAC-SHA256',
-    'issued_at' => 321,
-    'code' => 'foo_code',
-    'state' => 'foo_state',
-    'user_id' => 123,
-    'foo' => 'bar',
-  );
-
-  public function testValidSignedRequestsWillPassFormattingValidation()
+  protected function setUp()
   {
-    $sr = SignedRequest::make($this->payloadData, $this->appSecret);
-    SignedRequest::validateFormat($sr);
+    $this->fakeApp = m::mock('Facebook\Entities\FacebookApp', ['foo_app_id', 'foo_app_secret'])->makePartial();
   }
 
   /**
-   * @expectedException \Facebook\Exceptions\FacebookSDKException
+   * @expectedException Facebook\Exceptions\FacebookSDKException
+   * @expectedExceptionMessage Malformed signed request.
    */
-  public function testInvalidSignedRequestsWillFailFormattingValidation()
+  public function testThatConstructorThrowAnExceptionOnInvalidFormat()
   {
-    SignedRequest::validateFormat('invalid_signed_request');
-  }
-
-  public function testSignatureAndPayloadCanBeSeparatedInSignedRequests()
-  {
-    list($sig, $payload) = SignedRequest::split('sig.payload');
-
-    $this->assertEquals('sig', $sig);
-    $this->assertEquals('payload', $payload);
-  }
-
-  public function testBase64EncodingIsUrlSafe()
-  {
-    $encodedData = SignedRequest::base64UrlEncode('aijkoprstADIJKLOPQTUVX1256!)]-:;"<>?.|~');
-
-    $this->assertEquals('YWlqa29wcnN0QURJSktMT1BRVFVWWDEyNTYhKV0tOjsiPD4_Lnx-', $encodedData);
-  }
-
-  public function testAUrlSafeBase64EncodedStringCanBeDecoded()
-  {
-    $decodedData = SignedRequest::base64UrlDecode('YWlqa29wcnN0QURJSktMT1BRVFVWWDEyNTYhKV0tOjsiPD4/Lnx+');
-
-    $this->assertEquals('aijkoprstADIJKLOPQTUVX1256!)]-:;"<>?.|~', $decodedData);
-  }
-
-  public function testAValidEncodedSignatureCanBeDecoded()
-  {
-    $decodedSig = SignedRequest::decodeSignature('c2ln');
-
-    $this->assertEquals('sig', $decodedSig);
+    new SignedRequest($this->fakeApp, 'invalid_signed_request');
   }
 
   /**
-   * @expectedException \Facebook\Exceptions\FacebookSDKException
+   * @expectedException Facebook\Exceptions\FacebookSDKException
+   * @expectedExceptionMessage Signed request contains malformed base64 encoding.
    */
-  public function testAnImproperlyEncodedSignatureWillThrowAnException()
+  public function testThatConstructorThrowAnExceptionOnInvalidBase64InSig()
   {
-    SignedRequest::decodeSignature('foo!');
-  }
-
-  public function testAValidEncodedPayloadCanBeDecoded()
-  {
-    $decodedPayload = SignedRequest::decodePayload('WyJwYXlsb2FkIl0=');
-
-    $this->assertEquals(array('payload'), $decodedPayload);
+    new SignedRequest($this->fakeApp, 'foo!.foo');
   }
 
   /**
-   * @expectedException \Facebook\Exceptions\FacebookSDKException
+   * @expectedException Facebook\Exceptions\FacebookSDKException
+   * @expectedExceptionMessage Signed request has an invalid signature.
    */
-  public function testAnImproperlyEncodedPayloadWillThrowAnException()
+  public function testThatConstructorThrowAnExceptionWhenSignatureDontMatch()
   {
-    SignedRequest::decodePayload('foo!');
-  }
-
-  public function testSignedRequestDataMustContainTheHmacSha256Algorithm()
-  {
-    SignedRequest::validateAlgorithm($this->payloadData);
+    // Payload is {}
+    // Secret is foo_app_secret
+    new SignedRequest($this->fakeApp, 'fake_signature.e30=');
   }
 
   /**
-   * @expectedException \Facebook\Exceptions\FacebookSDKException
+   * @expectedException Facebook\Exceptions\FacebookSDKException
+   * @expectedExceptionMessage Signed request contains malformed base64 encoding.
    */
-  public function testNonApprovedAlgorithmsWillThrowAnException()
+  public function testThatConstructorThrowAnExceptionOnInvalidBase64InPayload()
   {
-    $signedRequestData = $this->payloadData;
-    $signedRequestData['algorithm'] = 'FOO-ALGORITHM';
-    SignedRequest::validateAlgorithm($signedRequestData);
-  }
-
-  public function testASignatureHashCanBeGeneratedFromBase64EncodedData()
-  {
-    $hashedSig = SignedRequest::hashSignature('WyJwYXlsb2FkIl0=', $this->appSecret);
-
-    $expectedSig = base64_decode('bFofyO2sERX73y8uvuX26SLodv0mZ+Zk18d8b3zhD+s=');
-    $this->assertEquals($expectedSig, $hashedSig);
-  }
-
-  public function testTwoBinaryStringsCanBeComparedForSignatureValidation()
-  {
-    $hashedSig = base64_decode('bFofyO2sERX73y8uvuX26SLodv0mZ+Zk18d8b3zhD+s=');
-    SignedRequest::validateSignature($hashedSig, $hashedSig);
+    new SignedRequest($this->fakeApp, 'X1hlZfmdY2S46nJmQcwy3EINPp9Wmb8O/KeKCP6d4Vs=.foo!');
   }
 
   /**
-   * @expectedException \Facebook\Exceptions\FacebookSDKException
+   * @expectedException Facebook\Exceptions\FacebookSDKException
+   * @expectedExceptionMessage Signed request has malformed encoded payload data.
    */
-  public function testNonSameBinaryStringsWillThrowAnExceptionForSignatureValidation()
+  public function testThatConstructorThrowAnExceptionWhenPayloadNotAnArray()
   {
-    $hashedSig1 = base64_decode('bFofyO2sERX73y8uvuX26SLodv0mZ+Zk18d8b3zhD+s=');
-    $hashedSig2 = base64_decode('GJy4HzkRtCeZA0cJjdZJtGfovcdxgl/AERI20S4MY7c=');
-    SignedRequest::validateSignature($hashedSig1, $hashedSig2);
-  }
-
-  public function testASignedRequestWillPassCsrfValidation()
-  {
-    SignedRequest::validateCsrf($this->payloadData, 'foo_state');
+    // Payload is false
+    // Secret is foo_app_secret
+    new SignedRequest($this->fakeApp, '5DBJNwmH_yDs0U2hh_mFN0Ak9jeS9wOC0tTUVFjxL9M=.ZmFsc2U=');
   }
 
   /**
-   * @expectedException \Facebook\Exceptions\FacebookSDKException
+   * @expectedException Facebook\Exceptions\FacebookSDKException
+   * @expectedExceptionMessage Signed request is using the wrong algorithm.
    */
-  public function testASignedRequestWithIncorrectCsrfDataWillThrowAnException()
+  public function testThatConstructorThrowAnExceptionWhenPayloadAsNoAlgorithm()
   {
-    SignedRequest::validateCsrf($this->payloadData, 'invalid_foo_state');
+    // Payload is {"foo":"bar"}
+    // Secret is foo_app_secret
+    new SignedRequest($this->fakeApp, '5xd-RDCU-sElaRQZAneUOtjBkV9edbnIL1NjqJMoXpU=.eyJmb28iOiJiYXIifQ==');
   }
 
-  public function testARawSignedRequestCanBeValidatedAndDecoded()
+  /**
+   * @expectedException Facebook\Exceptions\FacebookSDKException
+   * @expectedExceptionMessage Signed request is using the wrong algorithm.
+   */
+  public function testThatConstructorThrowAnExceptionWhenPayloadAsWrongAlgorithm()
   {
-    $payload = SignedRequest::parse($this->rawSignedRequest, 'foo_state', $this->appSecret);
+    // Payload is {"algorithm":"FOO-ALGORITHM"}
+    // Secret is foo_app_secret
+    new SignedRequest($this->fakeApp, 'T6yWrksHFBsAi-cgJp2KLPqsiNSSexU3czlBiRaaeTs=.eyJhbGdvcml0aG0iOiJGT08tQUxHT1JJVEhNIn0=');
+  }
 
-    $this->assertEquals($this->payloadData, $payload);
+  /**
+   * @expectedException Facebook\Exceptions\FacebookSDKException
+   * @expectedExceptionMessage Signed request did not pass CSRF validation.
+   */
+  public function testThatConstructorThrowAnExceptionWhenPayloadAsWrongState()
+  {
+    // Payload is {"algorithm":"HMAC-SHA256","state":"foo_state"}
+    // Secret is foo_app_secret
+    new SignedRequest(
+      $this->fakeApp,
+      'C2nM6RwvQ4Q5vEPrtKsdPdMjxi1b5U2N4C9sDqAuuU4=.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsInN0YXRlIjoiZm9vX3N0YXRlIn0=',
+      'wrong_state'
+    );
   }
 
   public function testARawSignedRequestCanBeInjectedIntoTheConstructorToInstantiateANewEntity()
   {
-    $signedRequest = new SignedRequest($this->rawSignedRequest, 'foo_state', $this->appSecret);
+    $expectedPayloadData = array(
+      'oauth_token' => 'foo_token',
+      'algorithm' => 'HMAC-SHA256',
+      'issued_at' => 321,
+      'code' => 'foo_code',
+      'state' => 'foo_state',
+      'user_id' => 123,
+      'foo' => 'bar',
+    );
 
-    $rawSignedRequest = $signedRequest->getRawSignedRequest();
-    $payloadData = $signedRequest->getPayload();
-    $userId = $signedRequest->getUserId();
-    $hasOAuthData = $signedRequest->hasOAuthData();
+    $signedRequest = new SignedRequest(
+      $this->fakeApp,
+      'U0_O1MqqNKUt32633zAkdd2Ce-jGVgRgJeRauyx_zC8=.eyJvYXV0aF90b2tlbiI6ImZvb190b2tlbiIsImFsZ29yaXRobSI6IkhNQUMtU0hBMjU2IiwiaXNzdWVkX2F0IjozMjEsImNvZGUiOiJmb29fY29kZSIsInN0YXRlIjoiZm9vX3N0YXRlIiwidXNlcl9pZCI6MTIzLCJmb28iOiJiYXIifQ==',
+      'foo_state'
+    );
 
     $this->assertInstanceOf('\Facebook\Entities\SignedRequest', $signedRequest);
-    $this->assertEquals($this->rawSignedRequest, $rawSignedRequest);
-    $this->assertEquals($this->payloadData, $payloadData);
-    $this->assertEquals(123, $userId);
-    $this->assertTrue($hasOAuthData);
+    $this->assertEquals($expectedPayloadData, $signedRequest->getPayload());
+    $this->assertEquals(123, $signedRequest->getUserId());
   }
 
 }
