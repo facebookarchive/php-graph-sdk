ff0c33f84be2914c85e934a7459624a7267f417c
Mauricio <mauricio.kruijer@gmail.com>
2015-02-06 23:31:28 +0100

    getTimezone for GraphUser
    Since 'timezone' is considered a core element of the User object (and I need it) I've included it in GraphUser

--- src/Facebook/GraphUser.php
+++ src/Facebook/GraphUser.php
@@ -132,4 +132,13 @@ class GraphUser extends GraphObject
     return $this->getProperty('location', GraphLocation::className());
   }
 
+  /**
+   * Returns the timezone for the user as a int if present.
+   *
+   * @return string|null
+   */
+  public function getTimezone()
+  {
+    return $this->getProperty('timezone');
+  }
 }
