25692b6cf5d0d889db7875e189d9fefe483bc3c8
Steven Maguire <steven@stevenmaguire.com>
2015-02-09 19:28:52 -0600

    making Facbebook\PseudiRandomString namespace PSR2 compliant

--- src/Facebook/PseudoRandomString/McryptPseudoRandomStringGenerator.php
+++ src/Facebook/PseudoRandomString/McryptPseudoRandomStringGenerator.php
@@ -27,44 +27,42 @@ use Facebook\Exceptions\FacebookSDKException;
 
 class McryptPseudoRandomStringGenerator implements PseudoRandomStringGeneratorInterface
 {
+    use PseudoRandomStringGeneratorTrait;
 
-  use PseudoRandomStringGeneratorTrait;
+    /**
+     * @const string The error message when generating the string fails.
+     */
+    const ERROR_MESSAGE = 'Unable to generate a cryptographically secure '
+        .'pseudo-random string from mcrypt_create_iv(). ';
 
-  /**
-   * @const string The error message when generating the string fails.
-   */
-  const ERROR_MESSAGE = 'Unable to generate a cryptographically secure pseudo-random string from mcrypt_create_iv(). ';
-
-  /**
-   * @throws FacebookSDKException
-   */
-  public function __construct()
-  {
-    if ( ! function_exists('mcrypt_create_iv')) {
-      throw new FacebookSDKException(
-        static::ERROR_MESSAGE .
-        'The function mcrypt_create_iv() does not exist.'
-      );
-    }
-  }
-
-  /**
-   * @inheritdoc
-   */
-  public function getPseudoRandomString($length)
-  {
-    $this->validateLength($length);
-
-    $binaryString = mcrypt_create_iv($length, MCRYPT_DEV_URANDOM);
-
-    if ($binaryString === false) {
-      throw new FacebookSDKException(
-        static::ERROR_MESSAGE .
-        'mcrypt_create_iv() returned an error.'
-      );
+    /**
+     * @throws FacebookSDKException
+     */
+    public function __construct()
+    {
+        if (!function_exists('mcrypt_create_iv')) {
+            throw new FacebookSDKException(
+                static::ERROR_MESSAGE .
+                'The function mcrypt_create_iv() does not exist.'
+            );
+        }
     }
 
-    return $this->binToHex($binaryString, $length);
-  }
+    /**
+     * @inheritdoc
+     */
+    public function getPseudoRandomString($length)
+    {
+        $this->validateLength($length);
 
+        $binaryString = mcrypt_create_iv($length, MCRYPT_DEV_URANDOM);
+
+        if ($binaryString === false) {
+            throw new FacebookSDKException(
+                static::ERROR_MESSAGE .
+                'mcrypt_create_iv() returned an error.'
+            );
+        }
+        return $this->binToHex($binaryString, $length);
+    }
 }
--- src/Facebook/PseudoRandomString/PseudoRandomStringGeneratorTrait.php
+++ src/Facebook/PseudoRandomString/PseudoRandomStringGeneratorTrait.php
@@ -25,40 +25,38 @@ namespace Facebook\PseudoRandomString;
 
 trait PseudoRandomStringGeneratorTrait
 {
+    /**
+     * Validates the length argument of a random string.
+     *
+     * @param int $length The length to validate.
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function validateLength($length)
+    {
+        if (!is_int($length)) {
+            throw new \InvalidArgumentException(
+                'getPseudoRandomString() expects an integer for the string length'
+            );
+        }
 
-  /**
-   * Validates the length argument of a random string.
-   *
-   * @param int $length The length to validate.
-   *
-   * @throws \InvalidArgumentException
-   */
-  public function validateLength($length)
-  {
-    if ( ! is_int($length)) {
-      throw new \InvalidArgumentException(
-        'getPseudoRandomString() expects an integer for the string length'
-      );
+        if ($length < 1) {
+            throw new \InvalidArgumentException(
+                'getPseudoRandomString() expects a length greater than 1'
+            );
+        }
     }
 
-    if ($length < 1) {
-      throw new \InvalidArgumentException(
-        'getPseudoRandomString() expects a length greater than 1'
-      );
+    /**
+     * Converts binary data to hexadecimal of arbitrary length.
+     *
+     * @param string $binaryData The binary data to convert to hex.
+     * @param int $length The length of the string to return.
+     *
+     * @return string
+     */
+    public function binToHex($binaryData, $length)
+    {
+        return mb_substr(bin2hex($binaryData), 0, $length);
     }
-  }
-
-  /**
-   * Converts binary data to hexadecimal of arbitrary length.
-   *
-   * @param string $binaryData The binary data to convert to hex.
-   * @param int $length The length of the string to return.
-   *
-   * @return string
-   */
-  public function binToHex($binaryData, $length)
-  {
-    return mb_substr(bin2hex($binaryData), 0, $length);
-  }
-
 }
