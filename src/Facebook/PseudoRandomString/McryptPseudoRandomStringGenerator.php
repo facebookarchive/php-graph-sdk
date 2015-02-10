f110cc9e456e27818270b38e5069c5b8c1899b06
Steven Maguire <steven@stevenmaguire.com>
2015-02-10 11:20:00 -0600

    single line long error message string

--- src/Facebook/PseudoRandomString/McryptPseudoRandomStringGenerator.php
+++ src/Facebook/PseudoRandomString/McryptPseudoRandomStringGenerator.php
@@ -32,8 +32,7 @@ class McryptPseudoRandomStringGenerator implements PseudoRandomStringGeneratorIn
     /**
      * @const string The error message when generating the string fails.
      */
-    const ERROR_MESSAGE = 'Unable to generate a cryptographically secure '
-        .'pseudo-random string from mcrypt_create_iv(). ';
+    const ERROR_MESSAGE = 'Unable to generate a cryptographically secure pseudo-random string from mcrypt_create_iv(). ';
 
     /**
      * @throws FacebookSDKException
