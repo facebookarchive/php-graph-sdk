8b036b4b5b8c2149c8b773705aeeb46b323f7a43
Sammy Kaye Powers <sammyk@sammykmedia.com>
2016-08-09 06:25:17 -0500

    Fix bug with including polyfills.php

--- src/Facebook/autoload.php
+++ src/Facebook/autoload.php
@@ -32,7 +32,7 @@ if (version_compare(PHP_VERSION, '5.4.0', '<')) {
     throw new Exception('The Facebook SDK requires PHP version 5.4 or higher.');
 }
 
-require_once __DIR__ . 'polyfills.php';
+require_once __DIR__ . '/polyfills.php';
 
 /**
  * Register the autoloader for the Facebook SDK classes.
