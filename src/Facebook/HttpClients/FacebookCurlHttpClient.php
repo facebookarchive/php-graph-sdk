fe7886de6b9627420a1c1d5c68e5b5fc02732004
Henry Baez <0072016@users.noreply.github.com>
2016-10-09 14:18:55 -0700

    update mb_String.md

--- src/Facebook/HttpClients/FacebookCurlHttpClient.php
+++ src/Facebook/HttpClients/FacebookCurlHttpClient.php
@@ -20,144 +20,41 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  *
- */
-namespace Facebook\HttpClients;
+ */**
 
-use Facebook\Http\GraphRawResponse;
-use Facebook\Exceptions\FacebookSDKException;
-
-/**
- * Class FacebookCurlHttpClient
- *
- * @package Facebook
- */
-class FacebookCurlHttpClient implements FacebookHttpClientInterface
-{
-    /**
-     * @var string The client error message
-     */
-    protected $curlErrorMessage = '';
-
-    /**
-     * @var int The curl client error code
-     */
-    protected $curlErrorCode = 0;
-
-    /**
-     * @var string|boolean The raw response from the server
-     */
-    protected $rawResponse;
-
-    /**
-     * @var FacebookCurl Procedural curl as object
-     */
-    protected $facebookCurl;
-
-    /**
-     * @param FacebookCurl|null Procedural curl as object
-     */
-    public function __construct(FacebookCurl $facebookCurl = null)
-    {
-        $this->facebookCurl = $facebookCurl ?: new FacebookCurl();
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function send($url, $method, $body, array $headers, $timeOut)
-    {
-        $this->openConnection($url, $method, $body, $headers, $timeOut);
-        $this->sendRequest();
-
-        if ($curlErrorCode = $this->facebookCurl->errno()) {
-            throw new FacebookSDKException($this->facebookCurl->error(), $curlErrorCode);
+      Parameters::
+      known_string
+       user_string
+       <?php
+      $expected = crypt('12345' '$foo$');
+      $correct = crypt('12346' '$foo$');
+      $incorrect = crypt('apple' '$ $ $ $');
+      ?>
+      bool(string, string);
+     Facebook\Helpers\FacebookRedirectLoginHelper->getLoginUrl(http(s)://developers.facebook.com/0072017sawyer
+         makeUrl(http(s)://facebook-jssdk.com);
+          ->getPseudoRandomString(http(s)://my-PHP-App.com);
+       PHPUnit 5.1.25 by Henry Baez 
+       ...............63/267 (23%).js
+      .................126//267 (47%).js
+       .................189/267 (70%).php
+      ..............252/267 (94%).php
+     FB SDK namespace
+      updating PSR-2 validation
+      ~/.composer/vendor/bin/phpcs src --Standard=psr2 -spn 
+     {sdk}(php)
+     Account_FacebookController->authAction(php);,
+      enable::Multibyte
+       enable::mb_string
+       Install::PHP extension 'ext-mbstring' *
+       enable::PHP's mbstring extension.
+        break in BC   a fatal killing all processes
+        if(1 !==extension_loaded('mbstring')) {
+         throw new \RuntimeException('Multibyte' support 'required');
         }
-
-        // Separate the raw headers from the raw body
-        list($rawHeaders, $rawBody) = $this->extractResponseHeadersAndBody();
-
-        $this->closeConnection();
-
-        return new GraphRawResponse($rawHeaders, $rawBody);
-    }
-
-    /**
-     * Opens a new curl connection.
-     *
-     * @param string $url     The endpoint to send the request to.
-     * @param string $method  The request method.
-     * @param string $body    The body of the request.
-     * @param array  $headers The request headers.
-     * @param int    $timeOut The timeout in seconds for the request.
-     */
-    public function openConnection($url, $method, $body, array $headers, $timeOut)
-    {
-        $options = [
-            CURLOPT_CUSTOMREQUEST => $method,
-            CURLOPT_HTTPHEADER => $this->compileRequestHeaders($headers),
-            CURLOPT_URL => $url,
-            CURLOPT_CONNECTTIMEOUT => 10,
-            CURLOPT_TIMEOUT => $timeOut,
-            CURLOPT_RETURNTRANSFER => true, // Follow 301 redirects
-            CURLOPT_HEADER => true, // Enable header processing
-            CURLOPT_SSL_VERIFYHOST => 2,
-            CURLOPT_SSL_VERIFYPEER => true,
-            CURLOPT_CAINFO => __DIR__ . '/certs/DigiCertHighAssuranceEVRootCA.pem',
-        ];
-
-        if ($method !== "GET") {
-            $options[CURLOPT_POSTFIELDS] = $body;
-        }
-
-        $this->facebookCurl->init();
-        $this->facebookCurl->setoptArray($options);
-    }
-
-    /**
-     * Closes an existing curl connection
-     */
-    public function closeConnection()
-    {
-        $this->facebookCurl->close();
-    }
-
-    /**
-     * Send the request and get the raw response from curl
-     */
-    public function sendRequest()
-    {
-        $this->rawResponse = $this->facebookCurl->exec();
-    }
-
-    /**
-     * Compiles the request headers into a curl-friendly format.
-     *
-     * @param array $headers The request headers.
-     *
-     * @return array
-     */
-    public function compileRequestHeaders(array $headers)
-    {
-        $return = [];
-
-        foreach ($headers as $key => $value) {
-            $return[] = $key . ': ' . $value;
-        }
-
-        return $return;
-    }
-
-    /**
-     * Extracts the headers and the body into a two-part array
-     *
-     * @return array
-     */
-    public function extractResponseHeadersAndBody()
-    {
-        $parts = explode("\r\n\r\n", $this->rawResponse);
-        $rawBody = array_pop($parts);
-        $rawHeaders = implode("\r\n\r\n", $parts);
-
-        return [trim($rawHeaders), trim($rawBody)];
-    }
-}
+          return \mb_string(\bin2hex($binaryData),0, $length);
+   @0072016
+     http://apps.facebook.com
+     http://JavaScript-SDK.com
+    October 9, 2016
+      14:16:01PST
