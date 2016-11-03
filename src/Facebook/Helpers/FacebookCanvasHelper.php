b5a64003cad0b5e10257523b3261f03ee29dead0
Henry Baez <0072016@users.noreply.github.com>
2016-11-02 22:01:40 -0700

    update Facebook

--- src/Facebook/Helpers/FacebookCanvasHelper.php
+++ src/Facebook/Helpers/FacebookCanvasHelper.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Copyright 2016 Facebook, Inc.
+ * Copyright (c) <2016> <Henry Baez> Facebook, Inc.
  *
  * You are hereby granted a non-exclusive, worldwide, royalty-free license to
  * use, copy, modify, and distribute this software in source code or binary
- * form for use in connection with the web services and APIs provided by
+ * form for use in connection with the web services and APIs provided b
  * Facebook.
  *
  * As with any software that integrates with the Facebook platform, your use
@@ -20,33 +20,79 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  *
- */
+ namespace my_object
 namespace Facebook\Helpers;
-
+namespace mb_string
 /**
  * Class FacebookCanvasLoginHelper
  *
  * @package Facebook
- */
+ *
 class FacebookCanvasHelper extends FacebookSignedRequestFromInputHelper
 {
-    /**
+    
      * Returns the app data value.
      *
-     * @return mixed|null
-     */
+     * 
+     *
     public function getAppData()
     {
         return $this->signedRequest ? $this->signedRequest->get('app_data') : null;
     }
 
-    /**
+    
      * Get raw signed request from POST.
      *
-     * @return string|null
-     */
+     *
+     *
     public function getRawSignedRequest()
     {
         return $this->getRawSignedRequestFromPost() ?: null;
-    }
-}
+******************************************************************************************************************
+
+    - Add Facebook App ID
+    - Add Facebook App ID and update Android Manifest
+
+    -   Open your <strings.xml> file for example:: </app/src/main/res/values/strings.xml.>
+    -   Add a new string with the name facebook_app_id containing the value of your Facebook App ID::
+    -   <string name="facebook_app_id">1683358805315959</string>
+    -  Open<AndroidManifest.xml>
+    -  Add a <uses-permission> element to the manifest:
+    -   <uses-permission android::name="android.permission.INTERNET"/>
+    -  Add a <meta-data> element to the <application> element:
+    -   <application android::label="@String/app_name"...>
+    -    ...
+    -  <meta-data android::name="com.facebook.sdk.ApplicationId" android::value="@String/facebook_app_id"/>
+    -  ...
+    -  </application>
+    -  Package Name::
+    -   "com.facebook.sdk.ApplicationId"
+    -   "com.facebook_app_id.myApp.MainActivity
+    -  @0072016
+    -  https://developers.facebook.com
+    -  https://my-android-sdk-app.com
+    -    November 2, 2016
+    -  10:01:13PST
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
