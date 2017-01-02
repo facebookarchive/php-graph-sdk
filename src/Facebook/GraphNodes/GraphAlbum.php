e72810790afb7d195a7aba8b209789131a670891
freek <freek@spatie.be>
2017-01-02 22:36:32 +0100

    update copyright headers

--- src/Facebook/GraphNodes/GraphAlbum.php
+++ src/Facebook/GraphNodes/GraphAlbum.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2016 Facebook, Inc.
+ * Copyright 2017 Facebook, Inc.
  *
  * You are hereby granted a non-exclusive, worldwide, royalty-free license to
  * use, copy, modify, and distribute this software in source code or binary
