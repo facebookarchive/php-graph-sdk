0d8898d33956dad11a78b6aaa1ab620a6fe8b0bb
Nathanael d. Noblet <nathanael@noblet.ca>
2017-08-16 11:08:28 -0600

    Fix phpdoc @return instead of @returns
    PhpDoc expects @return, @returns was being used instead

--- src/SignedRequest.php
+++ src/SignedRequest.php
@@ -164,7 +164,7 @@ class SignedRequest
     /**
      * Splits a raw signed request into signature and payload.
      *
-     * @returns array
+     * @return array
      *
      * @throws FacebookSDKException
      */
@@ -182,7 +182,7 @@ class SignedRequest
      *
      * @param string $encodedSig
      *
-     * @returns string
+     * @return string
      *
      * @throws FacebookSDKException
      */
@@ -202,7 +202,7 @@ class SignedRequest
      *
      * @param string $encodedPayload
      *
-     * @returns array
+     * @return array
      *
      * @throws FacebookSDKException
      */
