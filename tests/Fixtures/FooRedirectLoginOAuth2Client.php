6a8580ed928880379742fe9a3228e21d6a6abc4a
Martin Stuecklschwaiger <martin.stuecklschwaiger@walls.io>
2018-01-09 13:18:45 +0100

    Correctly return an AccessToken instance
    The method (in OAuth2Client) is expected to return an AccessToken
    instance

--- tests/Fixtures/FooRedirectLoginOAuth2Client.php
+++ tests/Fixtures/FooRedirectLoginOAuth2Client.php
@@ -23,12 +23,13 @@
  */
 namespace Facebook\Tests\Fixtures;
 
+use Facebook\Authentication\AccessToken;
 use Facebook\Authentication\OAuth2Client;
 
 class FooRedirectLoginOAuth2Client extends OAuth2Client
 {
     public function getAccessTokenFromCode($code, $redirectUri = '', $machineId = null)
     {
-        return 'foo_token_from_code|' . $code . '|' . $redirectUri;
+        return new AccessToken('foo_token_from_code|' . $code . '|' . $redirectUri);
     }
 }
